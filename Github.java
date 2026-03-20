f4dddff161c6f5d7eb35ae2c1a80922d94f11088
geethanjali7570-gif <geethanjali7570@gmail.com>
2026-03-20 15:42:23 +0530

    Add files via upload

---
+++ Github.java
@@ -0,0 +1,13 @@
+class Github
+{
+void display()
+{
+System.out.println("Welcome to Github");
+System.out.println("4th sem CSE department");
+}
+public static void main (String [] args)
+{
+Github g=new Github();
+g.display();
+}
+}
